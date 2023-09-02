6479f4a1daa1cd22d5d132f2b7dde11bc8dfca63
kozae00 <rhwogus777@naver.com>
2023-09-02 15:33:30 +0900

    commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def solution(common):
+    if common[1] - common[0] == common[-1] - common[-2]:
+        return common[-1] + (common[1] - common[0])
+    
+    else:
+        return common[-1] * (common[1]//common[0])
+
+    answer = 0
+    
+    return answer 
